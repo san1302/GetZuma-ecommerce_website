fe574d24b94f930d8f978195da014068df847a78
Sanchit Agarwal <sanchitagarwal0332@gmail.com>
2023-02-15 20:58:41 +0530

    Render table with dynamic data fetched from api.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+export default function App() {
+  const [items, setItem] = useState([]);
 
-function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://fakestoreapi.com/products");
+      result
+        .json()
+        .then((resData) => {
+          setItem(resData);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-  
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Rating</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, idx) => {
+            return (
+              <tr key={idx}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.price}</td>
+                <td>{item.description}</td>
+                <td>{item.category}</td>
+                <td>{`${item.rating.rate} (${item.rating.count})`}</td>
+                <td>
+                  <img src={item.image} width = "250px" alt={idx} />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
